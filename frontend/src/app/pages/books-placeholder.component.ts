5b23468c2e83f15b5d80c264f740dbeed556dedf
Rob <mail.orb@gmail.com>
2026-09-14 14:05:11 +0200

    remove unneeded stubs

--- frontend/src/app/pages/books-placeholder.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-books-placeholder',
-  standalone: true,
-  template: `<p>Books</p>`,
-})
-export class BooksPlaceholderComponent {}
